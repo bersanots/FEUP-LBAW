1599c7a18f56dd751e02d18748201d2ebb263a58
up201606124 <diogo_alex9@hotmail.com>
2019-04-23 10:47:01 +0100

    Login page partly done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,29 +4,34 @@
 <form method="POST" action="{{ route('login') }}">
     {{ csrf_field() }}
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
+    <div id="login-card" class="container">
+        <legend style="padding: 10px; text-align: center; font-weight: bold; font-variant: normal;" id="auth-legend">Login</legend>
+        <div id="login-side" class="container">
+            <img id="login-img" src="img/camera.jpg">
+        </div>
+        <form id="login-form" class="auth-form">
+            <label for="email">E-mail</label>
+            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+            @if ($errors->has('email'))
+            <span class="error">
+                {{ $errors->first('email') }}
+            </span>
+            @endif
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
-            {{ $errors->first('password') }}
-        </span>
-    @endif
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" required>
+            @if ($errors->has('password'))
+            <span class="error">
+                {{ $errors->first('password') }}
+            </span>
+            @endif
+            <button type="submit">
+                Login
+            </button>
+            <a class="button button-outline" href="{{ route('register') }}">Register</a>
+        </form>
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
 
-    <button type="submit">
-        Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
+    </div>
 </form>
 @endsection
