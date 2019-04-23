f02b5a2ec97147b2cc6ec4bb055d64d0de052fdc
up201606124 <diogo_alex9@hotmail.com>
2019-04-23 11:52:13 +0100

    CSS needs fixing on login and register pages. Added images

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,34 +4,34 @@
 <form method="POST" action="{{ route('login') }}">
     {{ csrf_field() }}
 
-    <div id="login-card" class="container">
-        <legend style="padding: 10px; text-align: center; font-weight: bold; font-variant: normal;" id="auth-legend">Login</legend>
-        <div id="login-side" class="container">
-            <img id="login-img" src="img/camera.jpg">
+    <div id="auth" class="container">
+        <div id="auth-card" class="container">
+            <legend id="auth-legend">Login</legend>
+            <div id="login-side" class="auth-side">
+                <img id="img-login" src=" {{ asset('img/camera.jpg') }}">
+            </div>
+            <form id="login-form" class="auth-form">
+                <label for="email">E-mail</label>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                @if ($errors->has('email'))
+                <span class="error">
+                    {{ $errors->first('email') }}
+                </span>
+                @endif
+
+                <label for="password">Password</label>
+                <input id="password" type="password" name="password" required>
+                @if ($errors->has('password'))
+                <span class="error">
+                    {{ $errors->first('password') }}
+                </span>
+                @endif
+                <button type="submit">
+                    Login
+                </button>
+                <a class="button button-outline" href="{{ route('register') }}">Register</a>
+            </form>
         </div>
-        <form id="login-form" class="auth-form">
-            <label for="email">E-mail</label>
-            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-            @if ($errors->has('email'))
-            <span class="error">
-                {{ $errors->first('email') }}
-            </span>
-            @endif
-
-            <label for="password">Password</label>
-            <input id="password" type="password" name="password" required>
-            @if ($errors->has('password'))
-            <span class="error">
-                {{ $errors->first('password') }}
-            </span>
-            @endif
-            <button type="submit">
-                Login
-            </button>
-            <a class="button button-outline" href="{{ route('register') }}">Register</a>
-        </form>
-
-
     </div>
 </form>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,38 +2,47 @@
 
 @section('content')
 <form method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
+  {{ csrf_field() }}
 
-    <label for="name">Name</label>
-    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-    @if ($errors->has('name'))
-      <span class="error">
+  <div id="auth" class="container">
+    <div id="auth-card" class="container">
+      <legend id="auth-legend">Register</legend>
+      <div id="login-side" class="auth-side">
+        <img id="register-img" src="img/cinema1.jpg">
+      </div>
+      <form id="register-form" class="auth-form">
+
+        <label for="name">Username</label>
+        <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
+        @if ($errors->has('name'))
+        <span class="error">
           {{ $errors->first('name') }}
-      </span>
-    @endif
+        </span>
+        @endif
 
-    <label for="email">E-Mail Address</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-    @if ($errors->has('email'))
-      <span class="error">
+        <label for="email">E-Mail Address</label>
+        <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+        @if ($errors->has('email'))
+        <span class="error">
           {{ $errors->first('email') }}
-      </span>
-    @endif
+        </span>
+        @endif
 
-    <label for="password">Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-      <span class="error">
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" required>
+        @if ($errors->has('password'))
+        <span class="error">
           {{ $errors->first('password') }}
-      </span>
-    @endif
+        </span>
+        @endif
 
-    <label for="password-confirm">Confirm Password</label>
-    <input id="password-confirm" type="password" name="password_confirmation" required>
-
-    <button type="submit">
-      Register
-    </button>
-    <a class="button button-outline" href="{{ route('login') }}">Login</a>
+        <button type="submit">
+          Register
+        </button>
+        <a class="button button-outline" href="{{ route('login') }}">Login</a>
+      </form>
+    </div>
+  </div>
+  </div>
 </form>
 @endsection
