26d447e4c8535a543414b03ae1d3e5a9af1a4ebc
up201606124 <up201606124@fe.up.pt>
2019-05-05 17:25:41 +0100

    Changed text in faq and about

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -5,14 +5,34 @@
 @section('content')
 
 <div class="container bs-docs-section" style="padding-bottom: -30px; ">
-    <h2>Question?</h2>
-    <p>The answer shall be here</p>
-    <h2>Question?</h2>
-    <p>The answer shall be here</p>
-    <h2>Question?</h2>
-    <p>The answer shall be here</p>
-    <h2>Question?</h2>
-    <p>The answer shall be here</p>
+    <div class="faq-item">
+        <h2>What topics can I ask about here?</h2>
+        <p>Lights, Camera, Action is for Movie & TV enthusiasts and experts alike!</p>
+        <ul>
+            If your question covers
+            <li>Analysis of content or theme of all forms of movies and TV series</li>
+            <li>Questions about a movie or TV show's production</li>
+            <li>Questions relating to the film and TV industry</li>
+            <li>The works of a director/actor/writer related to movies & TV</li>
+        </ul>
+    </div>
+    <div>
+        <h2>What should I do when someone answers my question?</h2>
+        <ol>Decide if the answer is helpful, and then...
+            <li>Vote on it. Vote up answers that are helpful and well-researched, and vote down answers that are not. Other users will also vote on answers to your question.</li>
+            <li>Accept it. As the writer of the question, you have a special privilege: you may accept the answer that you believe is the best solution to your problem.</li>
+        </ol>
+        <p>Please do not add a comment on your question or on an answer to say "Thank you". Comments are meant for requesting clarification, leaving constructive criticism, or adding relevant but minor additional information – not for socializing. If you want to say "thank you," vote on or accept that person's answer, or simply pay it forward by providing a great answer to someone else's question.</p>
+    </div>
+    <div class="faq-item">
+        <h2>What does it mean when an answer is "accepted"?</h2>
+        <p>When a user receives a good answer to their question, that user has the option to "accept" an answer.</p>
+        <p>Accepting an answer is not meant to be a definitive and final statement indicating that the question has now been answered perfectly. It simply means that the author received an answer that worked for them personally. Not every user comes back to accept an answer, and of those who do, they might not change the accepted answer even if a newer, better answer comes along later.</p>
+    </div>
+    <div class="faq-item">
+        <h2>What if I see someone doing something bad?</h2>
+        <p>We actively moderate our community, but we need your help to do so. If you see behavior that is rude, offensive, unproductive, or otherwise inappropriate for the site, let us know. If you are being harassed, notice that someone else is being harassed, or have any other concerns, speak up. Anything consistently reported by our community members will be investigated, and we will always follow up.</p>
+    </div>
 </div>
 
 @endsection
